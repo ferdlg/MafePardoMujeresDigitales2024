ae02fb84b08c7de743ac83263882eb27d29adbdb
ferdlg <ferdlgpar@gmail.com>
2024-08-26 10:55:39 -0500

    calculo del costo total en el carrito

--- MARIA FERNANDA DELGADO PARDO/TALLERES/TALLER2/StoreProject/src/components/Cart.jsx
+++ MARIA FERNANDA DELGADO PARDO/TALLERES/TALLER2/StoreProject/src/components/Cart.jsx
@@ -2,17 +2,25 @@ import React from "react";
 
 const Cart = ({productsCart})=>
 {
+    const totalCost =productsCart.reduce((p, product)=>{
+        return p + product.price
+    }, 0)
     return(
         <div>
             <div>
                 <h1>Carrito</h1>
             </div>
-            {productsCart.map((product) => (
-            <div key={product.id}>
-            <p>{product.name}</p>
-            <p>${product.price}</p>
+            <div>
+                {productsCart.map((product) => (
+                <div key={product.id}>
+                <p>{product.name}</p>
+                <p>${product.price}</p>
+                </div>
+                ))}
             </div>
-      ))}
+            <div>
+                <p>Costo Total = {totalCost} </p>
+            </div>
         </div>
     )
 };
--- MARIA FERNANDA DELGADO PARDO/TALLERES/TALLER2/StoreProject/src/components/Title.jsx
+++ MARIA FERNANDA DELGADO PARDO/TALLERES/TALLER2/StoreProject/src/components/Title.jsx
@@ -1,11 +1,29 @@
 import React from "react";
-
+import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button} from "@nextui-org/react";
 
 const Title = ()=>{
     return(
-        <div id="div-title">
-            <h1>Tienda de productos</h1>
-        </div>
+        <Navbar position="static">
+            <NavbarContent className="hidden sm:flex gap-4" justify="center">
+                <div >
+                    <h1>Tienda de productos</h1>
+                </div>
+            </NavbarContent>
+            <NavbarContent justify="end">
+                <NavbarItem>
+                    <Link color="foreground" href="#">
+                    Carrito
+                    </Link>
+                </NavbarItem>
+            </NavbarContent>
+            <NavbarBrand>
+                <NavbarItem>
+                    <Link color="foreground" href="#">
+                        logo
+                    </Link>
+                </NavbarItem>
+            </NavbarBrand>
+        </Navbar>
     )
 }
 
