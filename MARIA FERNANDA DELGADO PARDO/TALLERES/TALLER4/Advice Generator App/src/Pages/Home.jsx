a791312879e05cec76c68a8c1ff1b797aa1385d7
ferdlg <ferdlgpar@gmail.com>
2024-09-30 18:33:43 -0500

    estilos

--- MARIA FERNANDA DELGADO PARDO/TALLERES/TALLER4/Advice Generator App/src/Pages/Home.jsx
+++ MARIA FERNANDA DELGADO PARDO/TALLERES/TALLER4/Advice Generator App/src/Pages/Home.jsx
@@ -1,21 +1,27 @@
 import { Layout } from "../components/Layout";
 import { Card } from "../components/Card/Index";
 import { useFetch } from "../Hooks/useFetch";
+import { useState } from "react";
 
 export const Home = ()=>{
-    const {data, loading, error} = useFetch('https://api.adviceslip.com/advice');
+    const[url, setUrl] = useFetch('https://api.adviceslip.com/advice')
+    const {data, loading, error} = useFetch(url);
     if(loading){
         return ("Loading...")
     }
     if(error){
         return ("Error:", error.message)
     }
+    const handleClick = ()=>{
+        setUrl('https://api.adviceslip.com/advice')
+    };
     
     console.log(data.slip)
     return(
         <Layout>
             <Card
             slip = {data.slip}
+            handleClick = {handleClick}
             />
         </Layout>
     )
--- MARIA FERNANDA DELGADO PARDO/TALLERES/TALLER4/Advice Generator App/src/components/Card/Index.jsx
+++ MARIA FERNANDA DELGADO PARDO/TALLERES/TALLER4/Advice Generator App/src/components/Card/Index.jsx
@@ -1,19 +1,23 @@
+import { useFetch } from '../../Hooks/useFetch';
 import './Index.css';
-export const Card = (props) =>{
-    const {slip} = props
-    return(
-        <article className="Card">
-            <section className="Card-Section">
-                <h5>ADVICE #{slip.id}</h5>
-                <p>"{slip.advice}"</p>
-            </section>
-            <section className="Card-Section">
-                <figure className='Card-Section-Figure'>
-                    <img src ="src/assets/images/pattern-divider-desktop.svg" id='desktop' alt="divider-desktop" />
-                    <img src ="src/assets/images/pattern-divider-mobile.svg" id='mobile' alt="divider-mobile"/>
-                    <img src="src/assets/images/icon-dice.svg" alt="icon-dado" id='dado' />
+
+export const Card = (props) => {
+    const { slip , handleClick } = props;
+    
+
+    return (
+        <article className="card">
+            <section className="card__section">
+                <h6 className="card__advice-title">ADVICE #{slip.id}</h6>
+                <p className="card__advice-text">"{slip.advice}"</p>
+                <figure className='card__figure'>
+                    <img src="src/assets/images/pattern-divider-mobile.svg" className='card__figure-image--mobile' alt="divider-mobile" />
+                    <img src="src/assets/images/pattern-divider-desktop.svg" className='card__figure-image--desktop' alt="divider-desktop" />
                 </figure>
+            <button className='card__button' onClick={handleClick}>
+                <img src="src/assets/images/icon-dice.svg" className='card__button-icon' alt="icon-dado" />
+            </button>
             </section>
         </article>
-    )
-}
+    );
+};
