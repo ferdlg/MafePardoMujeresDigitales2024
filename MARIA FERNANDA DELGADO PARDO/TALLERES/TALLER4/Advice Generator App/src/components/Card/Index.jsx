c8b24fcf07d010a6fa6fa10a5f2f4b638c243d31
ferdlg <ferdlgpar@gmail.com>
2024-09-30 19:48:21 -0500

    ...

--- MARIA FERNANDA DELGADO PARDO/TALLERES/TALLER4/Advice Generator App/src/components/Card/Index.jsx
+++ MARIA FERNANDA DELGADO PARDO/TALLERES/TALLER4/Advice Generator App/src/components/Card/Index.jsx
@@ -7,18 +7,18 @@ export const Card = ({id, description, fetchData}) =>{
     };
     console.log('fetchData',fetchData)
     return(
-        <article className="Card">
-            <section className="Card-Section">
-                <h5>ADVICE #{id}</h5>
-                <p>"{description}"</p>
-            </section>
-            <section className="Card-Section">
-                <figure className='Card-Section-Figure'>
-                    <img src ="src/assets/images/pattern-divider-desktop.svg" id='desktop' alt="divider-desktop" />
-                    <img src ="src/assets/images/pattern-divider-mobile.svg" id='mobile' alt="divider-mobile"/>
+        <article className="card">
+            <section className="card__section">
+                <h5 className='card__advice-title'>ADVICE #{id}</h5>
+                <div className='card_container'>
+                    <p className='card__container-text' >"{description}"</p>
+                </div>
+                <figure className='card__figure'>
+                    <img className='card__figure-image--desktop' src ="src/assets/images/pattern-divider-desktop.svg" id='desktop' alt="divider-desktop" />
+                    <img className='card__figure-image--mobile' src ="src/assets/images/pattern-divider-mobile.svg" id='mobile' alt="divider-mobile"/>
                 </figure>
-                <button onClick={()=> handleClick()}>
-                    <img src="src/assets/images/icon-dice.svg" alt="icon-dado" id='dado' />
+                <button className='card__button' onClick={()=> handleClick()}>
+                    <img className='card__button-icon' src="src/assets/images/icon-dice.svg" alt="icon-dado" id='dado' />
                 </button>
             </section>
         </article>
